48d415a9b102eaff74d698a19d3dfc926802ca02
Ilya <ultraprolid@gmail.com>
2017-11-13 01:01:30 +0300

    indnaskj

---
+++ functional-background/play.js
@@ -0,0 +1,31 @@
+const fn = require('./../lib/es6-functional');
+import forEach from './../lib/import-test';
+
+let array = [1, 2, 3];
+let objectO = {
+    a: 1,
+    b: 2,
+    c: 3
+};
+
+forEach(array, (number) => console.log(number) );
+fn.forEach(array, (number) => console.log(number) );
+fn.forEachObject(objectO, (key, value) => console.log(key + " : " + value));
+fn.unless((10 < 5),
+    () => {console.log('You are wrong')}
+);
+
+fn.forEach([1,2,3,4,5,6,7], (number) => {
+    fn.unless((number % 2),
+        () => console.log(number, " is even")
+    )
+});
+
+fn.times(10, (number) => {
+    fn.unless(number % 2,
+        () => console.log(number, ' is even')
+    )
+});
+
+console.log(fn.every([1,2,3,4,5,6], (number) => {return number > 6}));
+console.log(fn.some([1,2,3,4,5,6,7], (number) => {return number > 6}));
