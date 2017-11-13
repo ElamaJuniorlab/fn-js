179d5cdeafdee441ddc449da051c0066057e21fc
lavandosovich <ultraprolid@gmail.com>
2017-11-13 03:09:38 +0300

    Add memoized, tap, once HOFs

--- functional-background/play.js
+++ functional-background/play.js
@@ -1,5 +1,5 @@
 const fn = require('./../lib/es6-functional');
-import forEach from './../lib/import-test';
+// import forEach from './../lib/import-test'; js only
 
 let array = [1, 2, 3];
 let objectO = {
@@ -8,7 +8,9 @@ let objectO = {
     c: 3
 };
 
-forEach(array, (number) => console.log(number) );
+fn.tap("Gold is cold")((value) => console.log("value is", value));
+fn.tap("Gold is cold")(false);
+// forEach(array, (number) => console.log(number) );
 fn.forEach(array, (number) => console.log(number) );
 fn.forEachObject(objectO, (key, value) => console.log(key + " : " + value));
 fn.unless((10 < 5),
@@ -29,3 +31,13 @@ fn.times(10, (number) => {
 
 console.log(fn.every([1,2,3,4,5,6], (number) => {return number > 6}));
 console.log(fn.some([1,2,3,4,5,6,7], (number) => {return number > 6}));
+
+let memoizedFactorial = fn.memoized((n) => {
+    if (n === 0) {
+        return 1;
+    }
+
+    return n * memoizedFactorial(n-1);
+});
+
+console.log(memoizedFactorial(5));
